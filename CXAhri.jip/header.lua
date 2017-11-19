4255765aa44ff469278703984bb1e0c92c36ac0a
Coozbie <33816052+Coozbie@users.noreply.github.com>
2017-11-19 20:41:18 -0300

    Create header.lua

---
+++ CXAhri.jip/header.lua
@@ -0,0 +1,8 @@
+return {
+  ["scriptType"] = "Champion",
+  ["scriptName"] = "Cyrex Ahri",
+  ["moduleName"] = "ahrigod",
+  ["loadToCoreMenu"] = function()
+    return player.charName == "Ahri"
+  end
+}
