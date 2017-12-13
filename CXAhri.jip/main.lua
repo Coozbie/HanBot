ed98c20c67d1d6c7f1cd4ac793b3002a37e61833
Coozbie <33816052+Coozbie@users.noreply.github.com>
2017-12-13 11:57:18 -0300

    Add files via upload

--- CXAhri.jip/main.lua
+++ CXAhri.jip/main.lua
@@ -3,26 +3,15 @@ local version = "1.4"
 local alib = module.load("avada_lib")
 local common = alib.common
 local draw = alib.draw
-local ts = alib.targetSelector
 
+local _enemyHeroes
 local orb = module.internal("orb/main")
 local gpred = module.internal("pred/main")
-
-local enemies = common.GetEnemyHeroes()
 
 local qPred = { delay = 0.25, width = 80, speed = 1500, boundingRadiusMod = 1, collision = { hero = false, minion = false } }
 local ePred = { delay = 0.25, width = 55, speed = 1550, boundingRadiusMod = 1, collision = { hero = true, minion = true } }
 
-local igniteDmg = { 70, 90, 110, 130, 150, 170, 190, 210, 230, 250, 270, 290, 310, 330, 350, 370, 390, 410 } --"50 + (20 * myHero.level)"
-local igniteSlot = nil
-if player:spellslot(4).name == "SummonerDot" then
-	igniteSlot = 4
-elseif player:spellslot(5).name == "SummonerDot" then
-	igniteSlot = 5
-end
-
 local menu = menuconfig("ahrigod", "Cyrex Ahri")
-	ts = ts(menu, 1500)
 	menu:header("xd", "Cyrex Ahri")
 	menu:menu("keys", "Key Settings")
 		menu.keys:header("xd", "Where The Magic Happens")
@@ -58,10 +47,6 @@ local menu = menuconfig("ahrigod", "Cyrex Ahri")
 		menu.harass:boolean("w", "Use W", true)
 		menu.harass:boolean("e", "Use E", true)
 		menu.harass:slider("Mana", "Min. Mana Percent: ", 10, 0, 100, 10)
-	menu:menu("auto", "Automatic Settings")
-		menu.auto:header("xd", "Automatic Settings")
-		menu.auto:boolean("Ignite", "Auto Ignite", true)
-	ts:addToMenu()
 	menu:menu("draws", "Draw Settings")
 		menu.draws:header("xd", "Drawing Options")
 		menu.draws:boolean("q", "Draw Q Range", true)
@@ -70,41 +55,38 @@ local menu = menuconfig("ahrigod", "Cyrex Ahri")
 	menu:header("author", "Author: Coozbie")
 
 function OnTick()
-	if menu.keys.combo:get() then Combo() end
-	if menu.keys.harass:get() then Harass() end
-	if menu.auto.Ignite:get() then AutoIgnite() end
+	target = GetTarget()
+	if menu.keys.combo:get() and target then Combo() end
+	if menu.keys.harass:get() and target then Harass() end
 end
 
 function Combo()
 	if menu.keys.combo:get() then
-		local target = ts.target
-		if target and not target.isDead then
-			if menu.combo.e:get() and common.CanUseSpell(2) then
-				local seg = gpred.linear.get_prediction(ePred, target)
-				if seg and seg.startPos:dist(seg.endPos) < 935 then
-					if not gpred.collision.get_prediction(ePred, seg, target) then
-						game.cast("pos", 2, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
-					end
+		if menu.combo.e:get() and common.CanUseSpell(2) then
+			local seg = gpred.linear.get_prediction(ePred, target)
+			if seg and seg.startPos:dist(seg.endPos) < 935 then
+				if not gpred.collision.get_prediction(ePred, seg, target) then
+					game.cast("pos", 2, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
 				end
 			end
-			if menu.keys.StartE:get() and common.CanUseSpell(2) then return end
-			if common.GetDistance(player, target) <= 530 and menu.combo.w:get() and common.CanUseSpell(1) then
-				if common.HasBuff(target, "AhriSeduce") then
-					game.cast("self", 1)
-				elseif common.GetPercentHealth(target) <= 60 then
-					game.cast("self", 1)
-				end
-			end
-			if menu.combo.q:get() and common.CanUseSpell(0) then
-				local seg = gpred.linear.get_prediction(qPred, target)
-				if seg and seg.startPos:dist(seg.endPos) < 850 then
-					if not gpred.collision.get_prediction(qPred, seg, target) then
-						game.cast("pos", 0, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
-					end
-				end
-			end
-			common.DelayAction(function() CastR() end, 1)
 		end
+		if menu.keys.StartE:get() and common.CanUseSpell(2) then return end
+		if common.GetDistance(player, target) <= 530 and menu.combo.w:get() and common.CanUseSpell(1) then
+			if Charm == true then
+				game.cast("self", 1)
+			elseif common.GetPercentHealth(target) <= 60 then
+				game.cast("self", 1)
+			end
+		end
+		if menu.combo.q:get() and common.CanUseSpell(0) then
+			local seg = gpred.linear.get_prediction(qPred, target)
+			if seg and seg.startPos:dist(seg.endPos) < 850 then
+				if not gpred.collision.get_prediction(qPred, seg, target) then
+					game.cast("pos", 0, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
+				end
+			end
+		end
+		common.DelayAction(function() CastR() end, 1)
 	end
 	--[Items]--
 	if menu.combo.items.zhonya:get() and common.GetPercentHealth(player) <= menu.combo.items.itemhp:get() and CountEnemyHeroInRange(600) >= 1 then
@@ -136,27 +118,24 @@ end
 function Harass()
 	if menu.keys.harass:get() then
 		if player.par / player.maxPar * 100 >= menu.harass.Mana:get() then
-			local target = ts.target
-			if target and not target.isDead then
-				if menu.harass.e:get() and common.CanUseSpell(2) then
-					local seg = gpred.linear.get_prediction(ePred, target)
-					if seg and seg.startPos:dist(seg.endPos) < 950 then
-						if not gpred.collision.get_prediction(ePred, seg, target) then
-							game.cast("pos", 2, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
-						end
+			if menu.harass.e:get() and common.CanUseSpell(2) then
+				local seg = gpred.linear.get_prediction(ePred, target)
+				if seg and seg.startPos:dist(seg.endPos) < 950 then
+					if not gpred.collision.get_prediction(ePred, seg, target) then
+						game.cast("pos", 2, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
 					end
 				end
-				if common.GetDistance(player, target) <= 500 then
-					if menu.harass.w:get() and common.CanUseSpell(1) then
-						game.cast("self", 1)
-					end
+			end
+			if common.GetDistance(player, target) <= 500 then
+				if menu.harass.w:get() and common.CanUseSpell(1) then
+					game.cast("self", 1)
 				end
-				if menu.harass.q:get() and common.CanUseSpell(0) then
-					local seg = gpred.linear.get_prediction(qPred, target)
-					if seg and seg.startPos:dist(seg.endPos) < 850 then
-						if not gpred.collision.get_prediction(qPred, seg, target) then
-							game.cast("pos", 0, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
-						end
+			end
+			if menu.harass.q:get() and common.CanUseSpell(0) then
+				local seg = gpred.linear.get_prediction(qPred, target)
+				if seg and seg.startPos:dist(seg.endPos) < 850 then
+					if not gpred.collision.get_prediction(qPred, seg, target) then
+						game.cast("pos", 0, vec3(seg.endPos.x, game.mousePos.y, seg.endPos.y))
 					end
 				end
 			end
@@ -169,34 +148,6 @@ function CastR()
 		game.cast("pos", 3, vec3(game.mousePos))
 	end
 end
-
-
-
---[[function AutoIgnite()
-	local target = ts.target
-	if target and not target.isDead and menu.KillSteal.Ignite2:get() and common.IsValidTarget(target) and common.GetDistance(player, target) <= 600 and target.health <= igniteDmg[player.level] then
-		game.cast("obj", igniteSlot, target)
-	end
-end--]]
-
-function AutoIgnite()
-	for i, enemy in ipairs(enemies) do
-        if common.IsValidTarget(enemy) and common.GetDistance(player, enemy) <= 600 then
-            if not player.isDead and igniteSlot and menu.auto.Ignite:get() and common.CanUseSpell(igniteSlot) and igniteDmg[player.level] > enemy.health then game.cast("obj", igniteSlot, enemy) end
-        end
-    end
-end
-
---[[function AutoZhonya()
-	if menu.auto.zhonya:get() and common.GetPercentHealth(player) <= menu.combo.itemhp:get() then
-		for i = 6, 11 do
-	    	local item = player:spellslot(i).name
-	    	if item and item == "ZhonyasHourglass" then
-	    		game.cast("self", i)
-	    	end
-        end
-	end
-end--]]
 
 
 function CountEnemyHeroInRange(range)
@@ -217,16 +168,79 @@ end
 end
 ]]--
 
-function OnDraw()
-	if menu.draws.q:get() then
-		glx.world.circle(player.pos, 880, 1, draw.color.gold, 50)
+function OnUpdateBuff(buff, causer)
+	if buff and buff.valid and buff.owner and buff.owner.type == player.type and buff.owner.team == enum.team.enemy then
+		if buff.type == 22 or buff.name == "AhriSeduce" then
+			Charm = true
+		end
 	end
-	if menu.draws.e:get() then
-		glx.world.circle(player.pos, 975, 1, draw.color.golden_rod, 50)
+end
+
+function OnRemoveBuff(buff, causer)
+	if buff and buff.valid and buff.owner and buff.owner.type == player.type and buff.owner.team == enum.team.enemy then
+		if buff.type == 22 or buff.name == "AhriSeduce" then
+			Charm = false
+		end
+	end
+end 
+
+function GetDistance(p1, p2)
+	p2 = p2 or player;
+	return p1.path.serverPos:dist(p2.path.serverPos)
+end
+
+function GetTarget(range)
+	range = range or 900;
+	if orb.combat.target and not orb.combat.target.isDead and orb.combat.target.isTargetable
+	 and orb.combat.target.isInvulnerable and orb.combat.target.isMagicImmune and orb.combat.target.isVisible then
+		return orb.combat.target
+	else
+		local dist, closest = math.huge, nil;
+		for k, unit in pairs(GetEnemy()) do
+			local unit_distance = GetDistance(unit);
+			if not unit.isDead and unit.isVisible and unit.isTargetable 
+				and unit.isInvulnerable and unit.isMagicImmune and unit_distance <= range then
+				if unit_distance < dist then
+					closest = unit;
+					dist = unit_distance;
+				end
+			end
+		end
+		if closest then
+			return closest
+		end
+	end
+	return nil
+end
+
+function _enemy_init()
+	_enemyHeroes = {};
+	for i = 0, objmanager.enemies_n - 1 do
+		_enemyHeroes[#_enemyHeroes + 1] = objmanager.enemies[i];
+	end
+	return _enemyHeroes
+end
+
+function GetEnemy()
+	if _enemyHeroes then 
+		return _enemyHeroes 
+	else
+		return _enemy_init();
+	end
+end
+
+function OnDraw()
+	if menu.draws.q:get() and common.CanUseSpell(0) then
+		glx.world.circle(player.pos, 880, 1, draw.color.cyan, 500)
+	end
+	if menu.draws.e:get() and common.CanUseSpell(2) then
+		glx.world.circle(player.pos, 975, 2, draw.color.purple, 500)
 	end
 end
 
 callback.add(enum.callback.tick, function() OnTick() end)
 callback.add(enum.callback.draw, function() OnDraw() end)
+callback.add(enum.callback.recv.removebuff, OnRemoveBuff)
+callback.add(enum.callback.recv.updatebuff, OnUpdateBuff)
 
 print("Cyrex Ahri v"..version..": Loaded")
