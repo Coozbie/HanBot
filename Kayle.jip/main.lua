c09c79351399458555d307a0d4ad42abab8f2d83
Coozbie <33816052+Coozbie@users.noreply.github.com>
2018-01-22 21:14:09 -0300

    Add files via upload

--- Kayle.jip/main.lua
+++ Kayle.jip/main.lua
@@ -3,12 +3,16 @@ local version = "1.0"
 local alib = module.load("avada_lib")
 local common = alib.common
 local draw = alib.draw
+local ts = alib.targetSelector
 
 local orb = module.internal("orb/main")
+local enemies = common.GetEnemyHeroes()
+local ally = common.GetAllyHeroes()
 
 local QlvlDmg = {60, 110, 160, 210, 260}
 
 local menu = menuconfig("kayle", "Cyrex Kayle")
+	ts = ts(menu, 800)
 	menu:header("script", "Cyrex Kayle")
 	menu:menu("keys", "Key Settings")
 		menu.keys:header("xd", "Where The Magic Happens")
@@ -31,6 +35,15 @@ local menu = menuconfig("kayle", "Cyrex Kayle")
 		menu.combo:boolean("r", "Use R", true)
 		menu.combo:slider("rhp", "Min. HP% for R: ", 25, 0, 100, 5)
 		menu.combo:slider("rx", "Enemys Near: ", 1, 0, 5, 1)
+		menu.combo:header("xd", "Ally Settings")
+		menu.combo:menu("x", "Ally Selection")
+			 for i = 0, objmanager.allies_n - 1 do
+				local ally = objmanager.allies[i]
+				if ally ~= player then
+					menu.combo.x:boolean(ally.charName, "Revive: "..ally.charName, false) 
+				end
+			end
+		menu.combo:slider("ahp", "HP% To Revive Ally", 10, 0, 100, 10)
 
 	menu:menu("harass", "Harass Settings")
 		menu.harass:header("xd", "Harass Settings")
@@ -45,46 +58,54 @@ local menu = menuconfig("kayle", "Cyrex Kayle")
 		menu.auto:boolean("heal", "Use W to Heal", true)
 		menu.auto:slider("healx", "Min HP% to Heal", 70, 0, 100, 5)
 
+	ts:addToMenu()
+
 	menu:menu("draws", "Draw Settings")
 		menu.draws:header("xd", "Drawing Options")
 		menu.draws:boolean("q", "Draw Q Range", true)
+		menu.draws:boolean("e", "Draw E Range", true)
 	menu:header("version", "Version: 1.0")
 	menu:header("author", "Author: Coozbie")
 
 function OnTick()
-	target = GetTarget()
-	if menu.keys.combo:get() and target then Combo() end
-	if menu.keys.harass:get() and target then Harass() end
+	if menu.keys.combo:get() then Combo() end
+	if menu.keys.harass:get() then Harass() end
 	if menu.auto.uks:get() then KillSteal() end
 	if menu.keys.run:get() then Run() end
-	if menu.keys.ultself:get() and common.CanUseSpell(3) then game.cast("obj", 3, player) end
+	if menu.keys.ultself:get() and common.CanUseSpell(3) and common.GetPercentHealth() <= 50 then game.cast("obj", 3, player) end
 	Auto()
 end
 
 function Combo()
 	if menu.keys.combo:get() then
-		if menu.combo.q:get() then
-			CastQ(target)
-		end
-		if menu.combo.w:get() and GetDistance(target) >= 550 then
-			if player.par / player.maxPar * 100 >= menu.combo.wm:get() and common.GetPercentHealth(player) >= menu.combo.whp:get() then
-				CastW()
+		local target = ts.target
+		if target and not target.isDead and not common.HasBuffType(target, 17) then
+			if menu.combo.q:get() then
+				CastQ(target)
 			end
-		end
-		if menu.combo.e:get() and GetDistance(target) <= 510 then
-			CastE(target)
+			if menu.combo.w:get() and selector.get_dist(target) >= 550 then
+				if common.GetPercentPar() >= menu.combo.wm:get() and common.GetPercentHealth() >= menu.combo.whp:get() then
+					CastW()
+				end
+			end
+			if menu.combo.e:get() and selector.get_dist(target) <= 510 then
+				CastE(target)
+			end
 		end
 	end
 end
 
 function Harass()
 	if menu.keys.harass:get() then
-		if player.par / player.maxPar * 100 >= menu.harass.Mana:get() then
-			if menu.harass.q:get() then
-				CastQ(target)
-			end
-			if menu.harass.e:get() then
-				CastE(target)
+		local target = ts.target
+		if target and not target.isDead and not common.HasBuffType(target, 17) then
+			if common.GetPercentPar() >= menu.harass.Mana:get() then
+				if menu.harass.q:get() then
+					CastQ(target)
+				end
+				if menu.harass.e:get() then
+					CastE(target)
+				end
 			end
 		end
 	end
@@ -111,15 +132,16 @@ end
 
 function CastQ(target)
 	if common.CanUseSpell(0) then
-		if GetDistance(target) <= 650 then
+		if selector.get_dist(target) <= 650 then
 			game.cast("obj", 0, target)
 		end
 	end
 end
 
-
+--add Q range check so kayle doesnt walk across the map
+--.isDead already has hp check so no need for if hp == 0 then return end (alternative just use selector.valid_target(enemy) instead of 50 diff checks xd
 function KillSteal()
-	for i, enemy in pairs(GetEnemy()) do
+	for i, enemy in pairs(enemies) do
 		if not enemy.isDead and enemy.isVisible and enemy.isTargetable and menu.auto.uks:get() then
 			local hp = enemy.health;
 			if hp == 0 then return end
@@ -131,11 +153,19 @@ function KillSteal()
 end
 
 function Auto()
-	if menu.auto.heal:get() and common.GetPercentHealth(player) <= menu.auto.healx:get() and CountEnemyHeroInRange(500) >= 1 then
+	if menu.auto.heal:get() and common.GetPercentHealth() <= menu.auto.healx:get() and CountEnemyHeroInRange(600) >= 1 then
 		CastW()
 	end
-	if menu.combo.r:get() and CountEnemyHeroInRange(900) >= menu.combo.rx:get() and common.GetPercentHealth(player) <= menu.combo.rhp:get() then
+	if menu.combo.r:get() and #common.GetEnemyHeroesInRange(800, player) >= menu.combo.rx:get() and common.GetPercentHealth() < menu.combo.rhp:get() then
 		CastR()
+	end
+	if common.CanUseSpell(3) then
+		for i = 0, objmanager.allies_n - 1 do
+			local ally = objmanager.allies[i]
+			if selector.is_valid(ally) and menu.combo.x[ally.charName]:get() and common.GetPercentHealth(ally) <= menu.combo.ahp:get() and selector.get_dist(ally) < 900 and #common.GetEnemyHeroesInRange(800, player) >= 1 then
+				game.cast("obj", 3, ally)
+			end
+		end
 	end
 end
 
@@ -177,60 +207,16 @@ function CalcMagicDmg(target, amount, from)
 	return math.floor(amount)
 end
 
-function GetDistance(p1, p2)
-	p2 = p2 or player;
-	return p1.path.serverPos:dist(p2.path.serverPos)
-end
-
-function GetTarget(range)
-	range = range or 900;
-	if orb.combat.target and selector.valid_target(orb.combat.target) and not orb.combat.target.isDead and orb.combat.target.isTargetable
-	 and orb.combat.target.isInvulnerable and orb.combat.target.isMagicImmune and orb.combat.target.isVisible then
-		return orb.combat.target
-	else
-		local dist, closest = math.huge, nil;
-		for k, unit in pairs(GetEnemy()) do
-			local unit_distance = GetDistance(unit);
-			if not unit.isDead and unit.isVisible and unit.isTargetable 
-				and unit.isInvulnerable and unit.isMagicImmune and unit_distance <= range then
-				if unit_distance < dist then
-					closest = unit;
-					dist = unit_distance;
-				end
-			end
-		end
-		if closest then
-			return closest
-		end
-	end
-	return nil
-end
-
-function _enemy_init()
-	_enemyHeroes = {};
-	for i = 0, objmanager.enemies_n - 1 do
-		_enemyHeroes[#_enemyHeroes + 1] = objmanager.enemies[i];
-	end
-	return _enemyHeroes
-end
-
-function GetEnemy()
-	if _enemyHeroes then 
-		return _enemyHeroes 
-	else
-		return _enemy_init();
-	end
-end
---[End Spyk Credits]--
-
-
 function OnDraw()
 	if menu.draws.q:get() and common.CanUseSpell(0) then
 		glx.world.circle(player.pos, 650, 2, draw.color.cyan, 100)
+	end
+	if menu.draws.e:get() then
+		glx.world.circle(player.pos, common.GetAARange(player), 2, draw.color.green, 100)
 	end
 end
 
 callback.add(enum.callback.tick, function() OnTick() end)
 callback.add(enum.callback.draw, function() OnDraw() end)
 
-print("Khantum Phyzix v"..version..": Loaded")
+print("Cyrex Kayle v"..version..": Loaded")
